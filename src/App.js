6c55c3b0905d2cc4a8a16e35e87d436a739a0c98
Elmira2505 <uan2505@mail.ru>
2024-04-19 17:28:46 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+// import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react'
+
+
+
+function App() {
+  const [counter, setCounter] = useState(0)
+  const [isReflect, setIsReflect] = useState(false)
+
+  function minus(){
+     setCounter(counter - 1)
+      reflect()
+
+  }
+    function plus(){
+        setCounter(counter + 1)
+       reflect(counter)
+    }
+    function reset(){
+        setCounter(0)
+        setIsReflect(false)
+    }
+function reflect() {
+      console.log(counter)
+    if (counter <= 0) {
+        setIsReflect(false)
+    } else {
+        setIsReflect(true)
+    }
+}
+
+
+  return (
+    <div className="App">
+       <h1>Counter</h1>
+        <h2>liked ❤️  {counter}
+           {isReflect ? '     🙂' : '     🥲'}
+        </h2>
+        <div >
+            <button className="button"  onClick={minus} >Minus</button>
+            <button  className="button" onClick={plus}  >Plus</button>
+            <button className="button"  onClick={reset}>Reset</button>
+
+        </div>
+
+
+    </div>
+  );
+}
+
+export default App;
